8855926f894fd36535448e377739fdac055ab461
A_Nightails <a.nightails@gmail.com>
2025-07-10 13:12:13 -0500

    add text to html node converter and test

---
+++ src/converter.py
@@ -0,0 +1,20 @@
+from textnode import TextType, TextNode 
+from leafnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    match (text_node.text_type):
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text, None)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text, None)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text, None)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text, None)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": f"{text_node.url}"})
+        case TextType.IMAGE:
+            return LeafNode("img", None, {"src": f"{text_node.url}", "alt": f"{text_node.text}"})
+        case _:
+            raise Exception(f"{text_node} has invalid text_type")
+
