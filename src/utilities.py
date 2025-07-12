f7f5f63a633c8a157878e61a7620335a9ec053bd
A_Nightails <a.nightails@gmail.com>
2025-07-12 16:45:37 -0500

    fix edge case of in split_nodes_delimiter for empty string and sometime still add NONE text node

--- src/utilities.py
+++ src/utilities.py
@@ -27,8 +27,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type is not TextType.TEXT:
             final_nodes.append(node)
+            continue
         texts = node.text.split(delimiter)
         for i in range(len(texts)):
+            if texts[i] == "":
+                continue
             new_node = TextNode(texts[i], TextType.TEXT)
             if i % 2 != 0:
                 new_node.text_type = text_type
