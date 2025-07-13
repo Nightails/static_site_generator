9a09bcbeeecde51d3bb3bfd3920ede424575a238
A_Nightails <a.nightails@gmail.com>
2025-07-12 23:20:21 -0500

    add test for text to textnode function

--- src/test_utilities.py
+++ src/test_utilities.py
@@ -158,6 +158,26 @@ class TestSplitImagesAndLinks(unittest.TestCase):
         )
 
 
+class TestTextToTextNodes(unittest.TestCase):
+    def test_text_to_nodes(self):
+        nodes = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
 
